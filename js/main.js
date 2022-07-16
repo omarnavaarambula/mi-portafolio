8c2ef52d494d170a625c840bde80dbb208134c5b
omarnavaarambula <106714513+omarnavaarambula@users.noreply.github.com>
2022-07-16 00:37:35 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const iconBurger = document.querySelector("#icon_burger");
-const menu = document.querySelector("#menu");
-
-iconBurger.addEventListener("click", function () {
-    menu.classList.toggle("show__menu");
-});
